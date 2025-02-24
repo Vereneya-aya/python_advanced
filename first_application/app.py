eb1da7c2751a7c0383e34ece14efc7a861e49a65
VL <veranikalis11@gmail.com>
2025-02-24 13:14:17 +0100

    Добавлен .gitignore

--- first_application/app.py
+++ first_application/app.py
@@ -4,8 +4,20 @@ from datetime import datetime
 app = Flask(__name__)
 
 @app.route("/test")
-def dev_test_function():
+def test_function():
     return f"Это текстовая страничка. Ответ сгенерирован в {datetime.now()}"
+
+@app.route("/hello")
+def hello_function():
+    return f"Hello World. Ответ сгенерирован в {datetime.now()}"
+
+counter = 0
+
+@app.route("/counter")
+def count():
+    global counter
+    counter += 1
+    return f"Страница открыта {counter} раз(а)."
 
 if __name__ == "__main__":
     app.run(debug=True)
